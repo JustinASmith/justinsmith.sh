13e7d23e57af93b46568e5f669f74464247a5063
Justin Smith <justinasmith662@gmail.com>
2024-09-06 16:02:41 -0500

    remove noise

--- components/about.tsx
+++ components/about.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Noise } from "./noise";
 import Divider from "./divider";
 import Link from "next/link";
 
@@ -8,7 +7,6 @@ type Props = {};
 export default function About({}: Props) {
   return (
     <>
-      <Noise />
       <h2 className="text-left text-balance text-2xl lg:text-3xl font-semibold tracking-[-0.015em] text-white mb-2">
         Who Am I?
       </h2>
